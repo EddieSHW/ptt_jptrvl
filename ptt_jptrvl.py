0ddc914461873202ce20f6684103aa23c1423432
Eddie Wang <bw81118@gmail.com>
2022-06-05 17:49:47 +0800

    save to db function test

--- ptt_jptrvl.py
+++ ptt_jptrvl.py
@@ -2,16 +2,24 @@
 # coding: utf-8
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
 import json
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from pprint import pprint
+import psycopg2
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
+DB_NAME = 'jptrvl'
+DB_USER = 'postgres'
+DB_PASS = 'pttjp118'
+
+conn_string = f'host={DB_HOST} user={DB_USER} dbname={DB_NAME} password={DB_PASS} port={DB_PORT}'
 
 options = webdriver.ChromeOptions()
 # options.add_argument("--headless")              # 不開啟實體瀏覽器，背景執行
@@ -20,40 +28,45 @@ options.add_argument("--incognito")  # 開啟無痕模式
 options.add_argument("--disable-popup-blocking")  # 禁用彈出攔截
 options.add_argument("--disable-notifications")  # 取消通知
 
-service = Service('./chromedriver.exe')
-
-driver = webdriver.Chrome(
-    options=options,
-    service=service
-)
+# service = Service('./chromedriver.exe')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 listData = []
 
 
+# start_page = 7239
+# number = 2
+# end_page = start_page - number
+
 def visit():
-    driver.get('https://www.ptt.cc/bbs/Japan_Travel/index.html')
-    sleep(1)
+    for i in range(1):
+        url = 'https://www.ptt.cc/bbs/Japan_Travel/index' + str(7239 - i) + '.html'
+        driver.get(url)
+        sleep(1)
+        getListItem()
+        getItemDetail()
+        saveDb()
 
 
 def getListItem():
     posts = driver.find_elements(By.CSS_SELECTOR, 'div.r-ent')
     for post in posts:
-        strTitle = post.find_element(By.CSS_SELECTOR, 'div.title > a').get_attribute('innerText')
-        strLink = post.find_element(By.CSS_SELECTOR, 'div.title > a').get_attribute('href')
-        strAuthor = post.find_element(By.CSS_SELECTOR, 'div.meta > div.author').get_attribute('innerText')
-        strPushCount = "no pushs"
-        #         if post.find_element(By.CSS_SELECTOR, 'div.nrec span') != None:
-        #             strPushCount = post.find_element(By.CSS_SELECTOR, 'div.nrec span').get_attribute('innerText')
-        #         else:
-        #             continue
+        try:
+            strTitle = post.find_element(By.CSS_SELECTOR, 'div.title > a').get_attribute('innerText')
+            strLink = post.find_element(By.CSS_SELECTOR, 'div.title > a').get_attribute('href')
+            strAuthor = post.find_element(By.CSS_SELECTOR, 'div.meta > div.author').get_attribute('innerText')
+            strPushCount = post.find_element(By.CSS_SELECTOR, 'div.nrec').get_attribute('innerText')
 
-        listData.append({
-            "title": strTitle,
-            "link": strLink,
-            "push_count": strPushCount,
-            "author": strAuthor
-        })
-        sleep(1)
+            listData.append({
+                "title": strTitle,
+                "link": strLink,
+                "push_count": strPushCount,
+                "author": strAuthor
+            })
+            sleep(1)
+
+        except NoSuchElementException:
+            continue
 
 
 def getItemDetail():
@@ -61,21 +74,43 @@ def getItemDetail():
         driver.get(articleDict['link'])
         meta_lines = driver.find_elements(By.CSS_SELECTOR, 'div.article-metaline')
         strTime = meta_lines[2].find_element(By.CSS_SELECTOR, 'span.article-meta-value').get_attribute('innerText')
-        strContent = driver.find_element(By.CSS_SELECTOR, 'div#main-content').text
+        all_text = driver.find_element(By.CSS_SELECTOR, 'div#main-content').get_attribute('innerText')
+        pre_text = all_text.split('--')[0]
+        texts = pre_text.split('\n')
+        contents = texts[4:]
+        content = ''.join(contents)
+
         listData[index]['time'] = strTime
-        listData[index]['content'] = strContent
+        listData[index]['content'] = content
 
         sleep(1)
 
-    print(listData)
+    # pprint(listData)
 
 
 def saveJson():
-    pass
+    with open("pttjptrvl.json", "w", encoding="utf-8") as fp:
+        fp.write(json.dumps(listData, ensure_ascii=False, indent=2))
 
 
 def saveDb():
-    pass
+    conn = psycopg2.connect(conn_string)
+    print("Connect sucessfully!")
+
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO public.jptrvl_post(title, link, author, time, push_count, content)
+        VALUES (%(title)s, %(link)s, %(author)s, %(time)s, %(push_count)s, %(content)s);
+    ''', {'title': listData['title'], 'link': listData['link'], 'author': listData['author'], 'time': listData['time'],
+          'push_count': listData['push_count'], 'content': listData['content']})
+
+    print(f'[{listData["title"]}] Append sucessfully')
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
 
 
 def close():
@@ -84,6 +119,5 @@ def close():
 
 if __name__ == '__main__':
     visit()
-    getListItem()
-    getItemDetail()
+    # saveJson()
     close()
